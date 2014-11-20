2f05ddd489f5b8d750cb2b527c76d1fd3332724a
Simone Battiste-Alleyne <softe1988@gmail.com>
2014-11-20 09:23:03 -0500

    Added in some user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :boards
 	has_many :pins
+
+	validates :name, presence: true
+	# {in: 2..10} means length must be 2-10 characters..using range function
+	validates :name, length: {in: 2..10}
 end
